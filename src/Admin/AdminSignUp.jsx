196c0c2a09708484ae12b33274811a3c14243132
Mohammed-Zeghlache <mouhamedzeghlache7@gmail.com>
2026-06-12 21:42:31 +0200

    Update AdminSignUp.jsx

--- src/Admin/AdminSignUp.jsx
+++ src/Admin/AdminSignUp.jsx
@@ -62,7 +62,7 @@ const AdminSignUp = () => {
     setErrors({});
 
     try {
-      const response = await fetch('http://localhost:5000/api/admins/signup', {
+      const response = await fetch('https://netguard-dz-back.onrender.com/api/admins/signup', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
