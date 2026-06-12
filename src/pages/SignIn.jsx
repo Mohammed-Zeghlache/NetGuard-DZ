7b026513e8e3108cb09d04470bd055f2b97ecf8b
Mohammed-Zeghlache <mouhamedzeghlache7@gmail.com>
2026-06-12 21:54:40 +0200

    Update SignIn.jsx

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -52,7 +52,7 @@ const Signin = () => {
         setError("");
 
         try {
-            const response = await fetch('http://localhost:5000/api/users/signup', {
+            const response = await fetch('https://netguard-dz-back.onrender.com/api/users/signup', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
